1abcd1fdf4feb9d2366cf9237729bc6087757195
Lazaro Alvarez <99672593+thisislaz@users.noreply.github.com>
2023-05-02 22:36:47 -0500

    spring back end and react frontend api tutorial

---
+++ freecodecampmovies/freecodecampmovies/src/main/java/net/thisislaz/freecodecampmovies/FreeCodeCampMoviesApplication.java
@@ -0,0 +1,22 @@
+package net.thisislaz.freecodecampmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class FreeCodeCampMoviesApplication {
+
+	public static void main(String[] args) {
+
+
+
+
+		SpringApplication.run(FreeCodeCampMoviesApplication.class, args);
+
+
+
+
+	}
+}
---
+++ freecodecampmovies/freecodecampmovies/src/main/java/net/thisislaz/freecodecampmovies/MovieService.java
@@ -0,0 +1,22 @@
+package net.thisislaz.freecodecampmovies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> oneMovie(String imdbid) {
+        return movieRepository.findMovieByImdbId(imdbid);
+    }
+}
